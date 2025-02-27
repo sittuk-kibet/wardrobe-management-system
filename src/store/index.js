f0f7de24c13e72eb7b887b2b2a7ab005d492120b
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2025-02-27 13:28:47 +0300

    refine api urls in fetchClothing and addclothing

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
@@ -22,7 +22,7 @@ export default createStore({
   actions: {
     async fetchClothings({ commit }) {
       try {
-        const response = await fetch('http://localhost:8000/api/clothings');    ////to redefine this api
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/api/clothings`);
         const data = await response.json();
         commit('setClothings', data);
       } catch (error) {
@@ -31,23 +31,45 @@ export default createStore({
     },
     async addClothing({ commit }, clothing) {
       try {
-        const response = await fetch('http://localhost:8000/api/clothings', {       //// also this
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/api/clothings`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${localStorage.getItem('token')}`
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
           },
           body: JSON.stringify(clothing),
         });
-        const newClothing = await response.json();
-        commit('addClothing', newClothing);
+
+        if (response.ok) {
+          const newClothing = await response.json();
+          commit('addClothing', newClothing);
+        } else {
+          console.error('Failed to add clothing item');
+        }
       } catch (error) {
         console.error('Error adding clothing:', error);
       }
     },
     async logout({ commit }) {
-      localStorage.removeItem('token');
-      commit('setLoginStatus', false);
+      try {
+        // Call backend to logout the user and invalidate the token
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/api/logout`, {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+
+        if (response.ok) {
+          // If the response is successful, clear localStorage and Vuex state
+          localStorage.removeItem('token');
+          commit('setLoginStatus', false);
+        } else {
+          console.error('Failed to log out');
+        }
+      } catch (error) {
+        console.error('Error logging out:', error);
+      }
     }
   },
   getters: {
