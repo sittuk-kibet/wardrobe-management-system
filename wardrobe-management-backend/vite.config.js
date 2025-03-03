f5da146fa8fa9966ca5aba6723eac9c17a516db4
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2025-03-04 00:51:55 +0300

    fix: update vite.config.js

--- wardrobe-management-backend/vite.config.js
+++ wardrobe-management-backend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
+    plugins: [vue()],
     resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-        }
+      alias: {
+        '@': path.resolve(__dirname, 'resources/js'), // Adjust this line to your project
+      },
     },
-});
+    build: {
+      outDir: path.resolve(__dirname, 'public/build'), // Ensure built files go to the public directory
+    },
+  });
